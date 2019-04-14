e8e6b332629ad2fdfe14fea8338218a52db80307
Alexey Semikozov <alwexy@gmail.com>
2019-04-14 20:28:13 +0300

    React init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('here');
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from './root';
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root')
+);
