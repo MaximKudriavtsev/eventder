da76fa26a7107c29cbd3a3012dd17c40f5341584
Alexey Semikozov <alwexy@gmail.com>
2019-04-14 21:16:38 +0300

    Init eslint and prettier

--- src/root.js
+++ src/root.js
@@ -1,4 +1,4 @@
 import * as React from "react";
-import {root} from './root.scss';
+import { root } from "./root.scss";
 
-export default () => <span className={root}>Root element</span>
+export default () => <span className={root}>Root element</span>;
