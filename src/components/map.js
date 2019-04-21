19de249b9d85f6505cb0c75c8d89a1511a9d08bd
Alexey Semikozov <alwexy@gmail.com>
2019-04-21 16:53:04 +0300

    Add change viewport ability

--- src/components/map.js
+++ src/components/map.js
@@ -1,15 +1,37 @@
 import * as React from "react";
+import PropTypes from "prop-types";
 import MapGL from "@urbica/react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { root } from "./map.scss";
 
-export default () => (
+const Map = ({ viewport, onViewportChange, ...restProps }) => (
   <MapGL
     className={root}
     mapStyle="mapbox://styles/mapbox/light-v9"
     accessToken="pk.eyJ1IjoiZXZlbnRkZXIiLCJhIjoiY2p1andwZXBwMDZ2dDN6bDh3aGltbWtyYiJ9.3YFTGz3UOIZs5H_tbs6XYg"
-    latitude={37.78}
-    longitude={-122.41}
-    zoom={11}
+    onViewportChange={onViewportChange}
+    latitude={viewport.latitude}
+    longitude={viewport.longitude}
+    zoom={viewport.zoom}
+    {...restProps}
   />
 );
+
+export default Map;
+
+Map.propTypes = {
+  viewport: {
+    latitude: PropTypes.number,
+    longitude: PropTypes.number,
+    zoom: PropTypes.number
+  },
+  onViewportChange: PropTypes.func.isRequired
+};
+
+Map.defaultProps = {
+  viewport: {
+    latitude: PropTypes.number,
+    longitude: PropTypes.number,
+    zoom: PropTypes.number
+  }
+};
--- src/containers/main.js
+++ src/containers/main.js
@@ -9,14 +9,28 @@ class Main extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      viewport: {
+        latitude: 54.19,
+        longitude: 37.61,
+        zoom: 11
+      }
+    };
 
     // eslint-disable-next-line react/prop-types
     props.actions.getUserLocation();
   }
 
   render() {
-    return <Map />;
+    const { viewport: stateVieport } = this.state;
+    return (
+      <Map
+        viewport={stateVieport}
+        onViewportChange={viewport => {
+          this.setState(viewport);
+        }}
+      />
+    );
   }
 }
 
