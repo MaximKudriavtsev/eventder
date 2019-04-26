f43e3c337da01287cc4bf76f06d9fb770e5b979e
Alexey Semikozov <alwexy@gmail.com>
2019-04-26 09:23:50 +0300

    Add marker

--- src/components/map.js
+++ src/components/map.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import MapGL from '@urbica/react-map-gl';
+import MapGL, { Marker } from '@urbica/react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { root } from './map.scss';
+import data from '../../scraper/result.json';
 
 const Map = ({ viewport, onViewportChange, ...restProps }) => (
   <MapGL
@@ -14,7 +15,41 @@ const Map = ({ viewport, onViewportChange, ...restProps }) => (
     longitude={viewport.longitude}
     zoom={viewport.zoom}
     {...restProps}
-  />
+  >
+    {data.map(({ preview_url: previewUrl, location, id }) => (
+      <Marker
+        offset={[0, -66]}
+        draggable
+        key={id}
+        latitude={location.lat}
+        longitude={location.lng}
+      >
+        <svg width="120" height="167" viewBox="0 0 120 167" fill="none">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M60.3969 0.855835C60.2359 0.855835 60.0751 0.856587 59.9145 0.858087V0.858097C59.7543 0.856604 59.5939 0.855857 59.4333 0.855857C31.4859 0.855857 8.83008 23.5117 8.83008 51.4591C8.83008 61.6609 11.849 71.1576 17.0422 79.1046L59.915 149.774L59.915 102.062L59.911 102.06L59.9151 102.06L59.9191 102.06L59.915 102.062L59.915 149.774L102.786 79.108C107.98 71.1604 111 61.6624 111 51.4591C111 23.5117 88.3443 0.855835 60.3969 0.855835ZM59.9149 74.5144C72.2959 74.5144 82.3327 64.4776 82.3327 52.0966C82.3327 39.7156 72.2959 29.6788 59.9149 29.6788C47.5339 29.6788 37.4972 39.7156 37.4972 52.0966C37.4972 64.4776 47.5339 74.5144 59.9149 74.5144Z"
+            fill="#00B9CB"
+          />
+          <defs>
+            <clipPath id="cut-off-bottom">
+              <circle cx="45" cy="45" r="43.5" />
+            </clipPath>
+          </defs>
+
+          <image
+            x="1%"
+            y="0%"
+            width="87"
+            height="87"
+            clipPath="url(#cut-off-bottom)"
+            transform="translate(15, 6)"
+            xlinkHref={previewUrl}
+          />
+        </svg>
+      </Marker>
+    ))}
+  </MapGL>
 );
 
 export default Map;
