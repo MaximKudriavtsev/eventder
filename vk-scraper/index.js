84841791f70a430000e5e36a05f58e436a24365a
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-04-27 20:23:38 +0300

    Try to use vk api

---
+++ vk-scraper/index.js
@@ -0,0 +1,15 @@
+// const METHOD_NAME = 'photos.search';
+// const BASE_URL = 'https://api.vk.com/method/';
+// const SECURE_KEY = 'FJauDVsCAmnxKezmbKPr';
+// const SERVICE_ACCESS_KEY =
+//   'c30a3060c30a3060c30a3060d8c3607031cc30ac30a30609fceb79ec122235c06d15e84';
+
+// const makeQueryString = (methodName, parameters, accessToken) =>
+//   `https://api.vk.com/method/${methodName}?${parameters}&access_token=${accessToken}&v=5.95`;
+
+// Dubai = lat=25.151939&long=55.321418
+
+// Tula = lat=54.193037&long=37.617031
+
+// string for fetch
+// https://api.vk.com/method/photos.search?lat=54.193037&long=37.617031&radius=5000&access_token=c30a3060c30a3060c30a3060d8c3607031cc30ac30a30609fceb79ec122235c06d15e84&v=5.95
