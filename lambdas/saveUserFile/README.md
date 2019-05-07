6e9efffb013370bb02aec0a34173eec3fcb04e95
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-05-07 22:48:42 +0300

    Try to add save-user-file lambda

---
+++ lambdas/saveUserFile/README.md
@@ -0,0 +1,5 @@
+# save-user-file
+
+## create
+
+curl -X POST https://1qn7e34k46.execute-api.eu-central-1.amazonaws.com/dev/files --data '{ "file": "http://www.netlore.ru/upload/files/19/large_p19hom1f751nk1c40ml57hu2skj.jpg", "lat": "54.192985", "lng": "37.603594" }'
---
+++ lambdas/saveUserFile/handler.js
@@ -0,0 +1,69 @@
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
+
+const s3PublicUrl = 'https://s3.eu-central-1.amazonaws.com/eventder/';
+
+module.exports.save = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  // const data = JSON.parse(event.body);
+  // if (typeof data.text !== 'string') {
+  //   console.error('Validation Failed');
+  //   callback(null, {
+  //     statusCode: 400,
+  //     headers: { 'Content-Type': 'text/plain' },
+  //     body: 'Couldn\'t create the todo item.',
+  //   });
+  //   return;
+  // }
+
+  const id = uuid.v1();
+
+  const dynamoDBParams = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id,
+      display_url: `${s3PublicUrl}${id}.jpg`,
+      preview_url: `${s3PublicUrl}${id}.jpg`,
+      owner_id: 'owner-id',
+      taken_at_timestamp: timestamp,
+      location: {
+        lat: event.lat,
+        lng: event.lng,
+        name: 'location-name',
+        instagramId: 'instagram-id'
+      }
+    }
+  };
+
+  const s3Params = {
+    Bucket: process.env.BUCKET,
+    Key: id,
+    Body: event.file
+  };
+
+  s3.putObject(s3Params);
+
+  // write the todo to the database
+  dynamoDb.put(dynamoDBParams, error => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: "Couldn't create the file item."
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(dynamoDBParams.Item)
+    };
+    callback(null, response);
+  });
+};
