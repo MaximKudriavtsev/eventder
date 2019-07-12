3bc70272f532976e8c0467d748e726c629697b02
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-07-12 22:07:29 +0300

    Change router system to HashRouter

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Landing from '../components/landing/top';
 import Main from './main';
 import Auth from '../components/auth';
 import ErrorPage from '../components/error-page';
 
 export default () => (
-  <Router basename={process.env.BASE_NAME}>
+  <HashRouter basename={process.env.BASE_NAME}>
     <Switch>
       <Route path="/" exact component={Landing} />
       <Route path="/main" exact component={Main} />
-      <Route path="/auth" exact component={Auth} />
-      <Route path="*" exact component={ErrorPage} />
+      <Route path="/auth" component={Auth} />
+      <Route path="*" component={ErrorPage} />
     </Switch>
-  </Router>
+  </HashRouter>
 );
