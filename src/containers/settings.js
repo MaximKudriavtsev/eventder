6969f72e881bc8e0215b07067a6e5ec88e319ae5
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-07-14 12:51:08 +0300

    Refactor the Settings component

--- src/containers/settings.js
+++ src/containers/settings.js
@@ -1,20 +1,21 @@
-import * as React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as rootActions from '../actions/actions';
+import { setUserData as setUserDataAction } from '../actions/actions';
 import checkCookie from '../utils/check-cookie';
 
 /* eslint-disable react/prop-types */
-const Settings = React.memo(props => {
+const Settings = props => {
   const userData = checkCookie('userData');
-  if (userData && !props.userData) {
-    props.actions.setUserData(userData);
+  const { userData: storeUserData } = props;
+  if (userData && !storeUserData) {
+    props.setUserData(userData);
   }
 
   return null;
-});
+};
 
 export default connect(
-  x => x,
-  dispatch => ({ actions: bindActionCreators(rootActions, dispatch) })
+  () => ({}),
+  dispatch => ({
+    setUserData: userData => dispatch(setUserDataAction(userData))
+  })
 )(Settings);
