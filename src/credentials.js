d757c8681aff7f2cfb2d5a6611f3034c7193b3a1
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-09-27 21:22:58 +0300

    Fix cognito redirect

--- src/credentials.js
+++ src/credentials.js
@@ -1,6 +1,8 @@
+const SITE_URL = 'https://myjoinmap.com';
+
 export const COGNITO_APP_URL = 'eventder.auth.eu-central-1.amazoncognito.com';
 export const COGNITO_CLIENT_ID = '2cebga41r6o40otl3jpnurnef0';
 export const REDIRECT_URL = process.env.BASE_NAME
-  ? 'https://maximkudriavtsev.github.io/site/'
+  ? SITE_URL
   : 'http://localhost:3000/';
 export const APP_NAME = 'Join Map';
