457d320e9488bade0f7da581f59a6bc7157b930d
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-09-27 20:41:15 +0300

    Prepare to publish site

--- src/credentials.js
+++ src/credentials.js
@@ -3,4 +3,4 @@ export const COGNITO_CLIENT_ID = '2cebga41r6o40otl3jpnurnef0';
 export const REDIRECT_URL = process.env.BASE_NAME
   ? 'https://maximkudriavtsev.github.io/site/'
   : 'http://localhost:3000/';
-export const APP_NAME = 'Urban Pulse';
+export const APP_NAME = 'Join Map';
