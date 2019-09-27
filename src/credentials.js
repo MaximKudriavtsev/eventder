667dc259b59737a9ba56fd98dca47ec4928c0d3c
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-09-27 21:47:55 +0300

    Change name

--- src/credentials.js
+++ src/credentials.js
@@ -2,7 +2,7 @@ const SITE_URL = 'https://myjoinmap.com';
 
 export const COGNITO_APP_URL = 'eventder.auth.eu-central-1.amazoncognito.com';
 export const COGNITO_CLIENT_ID = '2cebga41r6o40otl3jpnurnef0';
-export const REDIRECT_URL = process.env.BASE_NAME
+export const REDIRECT_URL = process.env.PROD
   ? SITE_URL
   : 'http://localhost:3000/';
-export const APP_NAME = 'Join Map';
+export const APP_NAME = 'My Join Map';
